939e98d9900a5a6deeb8efe0ab554ace7c67ffd6
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-05-09 19:22:17 +0530

    added assert_filter_void

--- test/test_array_void.c
+++ test/test_array_void.c
@@ -2,6 +2,33 @@
 #include<stdio.h>
 #include "../array_void.h"
 
+void assert_filter_void(void) {
+  printf("Filter_void\n");
+  ArrayVoid_ptr src = create_void_array(3);
+  src->array[0] = save_number(5);
+  src->array[1] = save_number(6);
+  src->array[2] = save_number(8);
+  PredicateVoid predicates[] = {&is_even_num,&is_vowel};
+  ArrayVoid_ptr result = filter_void(src,predicates[0]);
+  assert(result->length == 2);
+  assert(*(int *)result->array[0] == 6);
+  assert(*(int *)result->array[1] == 8);
+  printf("Passed\n");
+
+  printf(" should give vowels in the list :\n");
+  src = create_void_array(5);
+  src->array[0] = save_character('A');
+  src->array[1] = save_character('b');
+  src->array[2] = save_character('C');
+  src->array[3] = save_character('d');
+  src->array[4] = save_character('e');
+  result = filter_void(src,predicates[1]);
+  assert(result->length == 2);
+  assert(*(int *)result->array[0] == 'A');
+  assert(*(int *)result->array[1] == 'e');
+  printf("Passed\n");
+};
+
 void assert_map_void(void) {
   printf("Map_Void\n");
   printf("should increment each number by one in the list\n");
@@ -32,7 +59,7 @@ void assert_map_void(void) {
 
 int main(void) {
   assert_map_void();
-  // assert_filter_void();
+  assert_filter_void();
   // assert_reduce_void();
   return 0;
 }
